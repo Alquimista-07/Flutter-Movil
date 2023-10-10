9efd8336300f7eb257f7b88f93ddd5a959c1a206
Alquimista-07 <jdca.07@gmail.com>
2023-10-10 15:37:58 -0500

    25. Mixins

---
+++ 2. Sección 2 - Introducción a Dart/01-dart-intro/10_mixins.dart
@@ -0,0 +1,12 @@
+// NOTA: Lectura recomendada sobre los mixins: https://medium.com/flutter-community/dart-what-are-mixins-3a72344011f3
+//       Los mixins son algo muy especial de Dart y Flutter ya que son muy utilizados. Fernando menciona que trabaja 
+//       bastante con los mixins especialmente porque él creo su propia librería de animaciones, y que vamos a conocer
+//       más adelante en el curso y él trabaja bastante con los mixins para hacer ese tipo de animaciones.
+//
+//       También en la url hay un ejemplo con una gráfica de jeraquías usando animales para explicar y hacer la analogía 
+//       de los mixins.
+//
+//       Pero cual sería el beneficio de los mixins y es que nosotros podemos tener clases especializadas que cumplan una 
+//       condición en particular, por ejemplo podríamos mandarle alguna función ó a alguna clase, una clase que implemente
+//       específicamente ese mixin o funcionalidad que nosotros queremos, por ejemplo no importa si es un pez, un pájaro o 
+//       un mamífero con tal implemente el mixin me va a funcionar.
