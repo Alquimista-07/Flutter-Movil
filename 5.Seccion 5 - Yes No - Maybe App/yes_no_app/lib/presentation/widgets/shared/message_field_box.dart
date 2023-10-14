a44696b001114f5ffb58b45e720a901009124c2d
Alquimista-07 <jdca.07@gmail.com>
2023-10-14 17:10:42 -0500

    74. Comportamiento del FormField

--- 5.Seccion 5 - Yes No - Maybe App/yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ 5.Seccion 5 - Yes No - Maybe App/yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -5,6 +5,14 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // NOTA: Este elemento TextEditingController como su nombre lo indica nos va a dar control sobre el input en el que lo voy a usar
+    //       y es algo que vamos a ver más a detalle cuando manejemos formularios más complejos
+    //       como un login entre otras cosas que veremos más adelante en el curso.
+    final textComtroller = TextEditingController();
+
+    // NOTA: Propiedad para mantener el foco cuando se envía el mensaje y que el teclado no se cierre
+    final focusNode = FocusNode();
+
     // NOTA: Propiedad para colocar el color y los bordes redondeadas en el input
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
@@ -12,6 +20,7 @@ class MessageFieldBox extends StatelessWidget {
 
     // NOTA: Estulos del input
     final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "?"',
         // NOTA: Borde
         enabledBorder: outlineInputBorder,
         // Borde cuando se tiene foco en el input
@@ -23,19 +32,39 @@ class MessageFieldBox extends StatelessWidget {
           icon: const Icon(Icons.send_outlined),
           // NOTA: Acción al presionar el botón
           onPressed: () {
-            print('Valor de la caja de texto');
+            // NOTA: Obtenemos el valor de la caja de texto al presionar el IconButton
+            final textValue = textComtroller.value.text;
+            print('button: $textValue');
+            // NOTA: Limpiamos la caja de texto
+            textComtroller.clear();
           },
         ));
 
     return TextFormField(
+      // NOTA: Propiedad para el comportamiento cuando hacemos click fuera del input
+      onTapOutside: (event) {
+        // NOTA: Entonces cuando haga click fuera del input perdemos el foco y cerramos el teclado
+        focusNode.unfocus();
+      },
+      // NOTA: Asignamos la propiedad para mantener el foco cuando se hace submit y no cerar el teclado
+      focusNode: focusNode,
+      // NOTA: Agregamos el controlador
+      controller: textComtroller,
       decoration: inputDecoration,
       // NOTA: Hay varias formas de mandar lo que se escribe en el input una es usando el onFieldSubmitted, otra el onChanged
       onFieldSubmitted: (value) {
-        print('Submit value $value');
+        //print('Submit value $value');
+        // NOTA: Limpiamos el valor del input cuando se envía
+        textComtroller.clear();
+        // NOTA: Luego de que se limpia siempre mantengo el foco y no se cierre el tecado
+        focusNode.requestFocus();
       },
+      // NOTA: Esta es otra forma de obtener el valor del input pero para este caso no lo vamos a ocupar
+      /*
       onChanged: (value) {
         print('Changed: $value');
       },
+      */
     );
   }
 }
