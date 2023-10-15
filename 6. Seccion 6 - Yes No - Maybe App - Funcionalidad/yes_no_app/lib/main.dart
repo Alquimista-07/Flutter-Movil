8438aaeba4dcd5ab2159f76d9fa4f7460a3bb473
Alquimista-07 <jdca.07@gmail.com>
2023-10-15 12:05:44 -0500

    81. Instalar Provider

--- 6. Seccion 6 - Yes No - Maybe App - Funcionalidad/yes_no_app/lib/main.dart
+++ 6. Seccion 6 - Yes No - Maybe App - Funcionalidad/yes_no_app/lib/main.dart
@@ -2,7 +2,9 @@
 //       proyecto es mateapp. Este ya nos autogenera el código base del widget con su appbar, el Scaffold
 //      y demás cosas y sobre el cual modificamos para empezar nuestra aplicación.
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -12,12 +14,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Yes No App',
-      // Usamos nuestra instancia de app_theme donde tenemos nuestro tema personalizado
-      theme: AppTheme(selectedColor: 3).theme(),
-      debugShowCheckedModeBanner: false,
-      home: const ChatScreen(),
+    // NOTA: Agregamos el provider
+    return MultiProvider(
+      providers: [
+        // NOTA: Este ChangeNotifier Provider espera que le pasemos una propiedad o método llamada create la cual espera que yo haga la creación
+        //       de la instancia inicial. Ahora porque le colocamos como guion bajo (_) a el parámetro, es porque ahí va el BuildContext pero si
+        //       no lo vamos a usar no podemos dejar el parámetro vacío, es decir, solo los paréntesis ya que necesitamos especificarlo pero pues
+        //       en este caso como no vamos a usarlo por eso lo indicamos solo con el guion bajo (_) y esto es bien común para indicar que no me
+        //       interesa ese argumento y ya con esto creamnos nuesto ChatProvider a nivel global de la aplicación, y es como si hubiera envuelto
+        //       toda mi aplicación y todos los widgets hijos va a tener acceso y poder consultar la información del provider.
+        // NOTA: Ya con esto se preseta para que ya en la parte superior donde aparece la barra del debug cuando ejecutamos la aplicación tengamos
+        //       un ícono similar a una lupa con el logo de Flutter que si lo presionamos nos va a abrir las DevTools y al cargar nos va a mostrar
+        //       el árbol de widgets completom, similar a las DevTools de React que nos muesta el API Context
+        ChangeNotifierProvider(create: (_) => ChatProvider())
+      ],
+      child: MaterialApp(
+        title: 'Yes No App',
+        // Usamos nuestra instancia de app_theme donde tenemos nuestro tema personalizado
+        theme: AppTheme(selectedColor: 3).theme(),
+        debugShowCheckedModeBanner: false,
+        home: const ChatScreen(),
+      ),
     );
   }
 }
--- 6. Seccion 6 - Yes No - Maybe App - Funcionalidad/yes_no_app/lib/presentation/providers/chat_provider.dart
+++ 6. Seccion 6 - Yes No - Maybe App - Funcionalidad/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -8,6 +8,29 @@
 //       para ordenar.
 //       Otra cosa que hay que mencionar es que Provider es uno de los gestores de estado Flutter Favorite
 
+// --------------------------------------------------------
+// NOTAS INSTALACIÓN GESTOR DE ESTADO - PROVIDER
+// --------------------------------------------------------
+// NOTA: Para realizar la instalación del gestor Provider necesitamos visitar pub.dev y buscarlo allí.
+//       Pub.dev es el sitio donde tenemos disponibles los paquetes desarrollados por el equipo de Flutter y la comunidad, es decir, allí podemos encontrar muchas cosas tanto gestores, como widgets y demás. Adicionalmente una cosa super importante es fijarnos para que plataforma nos
+//       va a servir ese paquete ya que siempre no va a estar disponible para todas las plataformas, en este caso como estamos desarrollado para Android y IOS nos fijamos que dichas plataformas estén soportadas.
+//
+//       Ahora directamente para realizar la instalación si nos dirigimos a la pestaña installing allí vamos a ver mostrar la forma de instalarlo directamente mediante Flutter que es una forma fácil de añadirlo y el cual nos agrega la dependencia. Entonces solo lo que tenemos que hacer
+//       es en la consola dentro del proyecto ejecutar el comando indicado.
+//
+//       Otra forma de instalarlo sería irnos directamente al archivo pubscpec.yaml y en la parte de las dependencias pegar la línea con la dependenia y la cual luego de grabar el archivo se va a descargar de forma automática.
+//
+//       Y otra forma es mediante la instalación de la extensión llamada Pubscpec Assist que instalamos anteriormente y la cual también nos ayuda a realizar la instalación de dependenicas, para usarlo simplemente vamos a la paleta de comando con Ctrl + Shift + P luego escribimos pub assist
+//       y luego colocamos en este caso provider y esto va a realizar la instalación y a ordenar.
+//
+//       Ahora Provider me va a permitir tener los ChangeNotifier de manera global o a un cierto nivel de nuestro contexto de la aplicación, es decir, podemos colcar la instancia para que este disponible solo este en un punto por ejemplo intermedio en el árbol de widget y por lo tanto solo los
+//       hijos que se desprendan o que dependan del que contiene el provider van a poder acceder al provider y los que estén por encima o afuera no lo van a poder hacer o no van a poder acceder al provider y no saben de su existencia. Por ejemplo podemos tener providers solo a nivel de formularios o
+//       a niveles de widgets propiamente, pero si queremos que el provider funcione en toda la aplicación debemos colocarlo a un nivel muy alto para que todos los todos su demás widgets hijos lo puedan ver.
+//
+//       Entonces para hacer eso nos dirigimos al main.dart y vamos a envolver el MaterialApp con un nuevo widget llamado MultiProvider pero hay varios widgets que provider que nos permiten proveer esa información, pero el MultiProvider es muy útil porque usualmente puede ser que tengamos más de un
+//       proveedor de información o más de una clase provider (que en este caso tenemos una que creamos dentro de la carpeta providers y llamamos chat_provider) que necesite compartir información a lo largo del mismo, por ejemplo una de autenticación, el chat, el tema o un gestor de temas, es decir,
+//       pueda ser que necesitemos muchos providers y el MultiProvider es genial para eso. Entonces al MultiProvider le pasamos una propiedad providers, que tiene la lista de los providers.
+
 import 'package:flutter/widgets.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
