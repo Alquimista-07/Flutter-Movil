d8750fa9134ffc53b58e68887d290b42df3e364d
Alquimista-07 <jdca.07@gmail.com>
2023-10-16 10:38:11 -0500

    95. Provider - Y problemática futura

--- 7. Seccion 7 - TokTik - Videos verticales/toktik/lib/main.dart
+++ 7. Seccion 7 - TokTik - Videos verticales/toktik/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,18 +11,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TokTik',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme().getTheme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    // NOTA: Envolvemos en el Multiprovider y le pasamos el o los providers. Esto se explico en la aplicación anterior (YesNoApp)
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => DiscoverProvider(),
+        )
+      ],
+      child: MaterialApp(
+          title: 'TokTik',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme().getTheme(),
+          home: const DiscoverScreen()),
     );
   }
 }
---
+++ 7. Seccion 7 - TokTik - Videos verticales/toktik/lib/presentation/providers/discover_provider.dart
@@ -0,0 +1,26 @@
+// Provider
+
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+class DiscoverProvider extends ChangeNotifier {
+  // NOTA: Creamos una bandera booleana para suponer que cuando cargo la aplicación no voy a tener ningún video
+  bool initialLoading = true;
+
+  List<VideoPost> videos = [];
+
+  // NOTA: Aunque solo tengamos una página igual quiero usar esta función para cargar los videos.
+  Future<void> loadNextPage() async {
+    // NOTA: Ahora un problema que deberíamos de empezar a ver es que cuando yo empiece y si hago toda la implementación de la función de cargar los videos en este punto, eso va a amarrar a mi provider a la dependencia del origien
+    //       de datos que va a ser local que es el origne de datos que tenemos en share/data/local_video_posts.dart y al día de mañana cuando yo diga no ya no voy a ocupar nada de eso, entonces voy a tener que venir al discover_provider
+    //       y hacer las modificaciones respectivas para cargar de un datasource diferente o una funete de datos diferente.
+    //       Pero esto es algo que vamos a terminar resolviendo más adelante ya que si no lo hacemos vamos a violentar varios principios SOLID, uno de ellos sería el Open and Closed que dice que nuestras funciones, clases tienen que estar
+    //       abiertas a la extensión pero cerradas a la modificación y para lograr esto nosotros deberíamos de poder recibir alguna función, algún tipo de repositorio, es decir, recibir un objeto en el cual yo sepa o ese objeto sepa como
+    //       y de donde cargar esos videos. Por lo tanto a este provider no le debería de importar de donde voy a cargar esos videos, al provider simplemente le debería de importar OK necesito llamar esta función para cargar los videos
+    //       del lugar u origen de datos que yo especifique.
+    // TODO: Cargar videos
+
+    // Notificamos del cambio de estado para que se renderice nuevamente.
+    notifyListeners();
+  }
+}
