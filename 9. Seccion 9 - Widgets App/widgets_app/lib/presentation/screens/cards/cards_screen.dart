f162c08eabbe45b8b31dd2fcedf1673900bd8798
Alquimista-07 <jdca.07@gmail.com>
2023-10-20 16:49:43 -0500

    129. Tarjetas con relleno e imágenes

--- 9. Seccion 9 - Widgets App/widgets_app/lib/presentation/screens/cards/cards_screen.dart
+++ 9. Seccion 9 - Widgets App/widgets_app/lib/presentation/screens/cards/cards_screen.dart
@@ -52,6 +52,14 @@ class _CardsView extends StatelessWidget {
 
           ...cards.map((card) =>
               _CardType2(label: card['label'], elevation: card['elevation'])),
+
+          ...cards.map((card) =>
+              _CardType3(label: card['label'], elevation: card['elevation'])),
+
+          ...cards.map((card) =>
+              _CardType4(label: card['label'], elevation: card['elevation'])),
+
+          const SizedBox(height: 40),
         ],
       ),
     );
@@ -127,6 +135,96 @@ class _CardType2 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    // NOTA: Usamos el widget para crear el card
+    return Card(
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            // NOTA: El widget Align como su nombre lo indica sirve para alinea o hacer alineación de algún elemento hijo
+            Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - Filled'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    // NOTA: Usamos el widget para crear el card
+    return Card(
+      // NOTA: Ahora como estamos usando una imágen vamos a perder los bordes redondeados de la tarjeta
+      //       entonces esto usamos la propiedad clipBehavior a la cual le asignamos el Clip.hardEdge
+      //       cuya función es evitar que los hijos se salgan de su contenedor padre.
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      // NOTA: A diferencia de los otros card a este le removemos el Padding, y para removerlo de forma fácil y rápida sin afectar
+      //       los hijos, simplemente nos paramos sobre el widget y luego le damos Ctrl + . y ahí nos da la opción de removerlo
+      // NOTA: Ahora no usamos un Column sino un Stack y recordemos que la diferencia es que el Stack permite colocar widgets
+      //       unos sobre otros, y por lo tanto el que este primero es el que va a estar más atrás, y entre más abajo estén en
+      //       el listado de children más al frente va a aestar.
+      child: Stack(
+        children: [
+          // NOTA: Cargamos una imágen desde una url de internet a la cual le asignamos un id a partir del elevation
+          //       y lo casteamos a entero, adicionalmente le colocamos unas dimensiones estáticas de 600 x 350
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            // NOTA: La propiedad fit especifica como queremos que la imágen se adapte a su espacio.
+            fit: BoxFit.cover,
+          ),
+
+          // NOTA: El widget Align como su nombre lo indica sirve para alinea o hacer alineación de algún elemento hijo
+          Align(
+            alignment: Alignment.centerRight,
+            // NOTA: Usamos un Contanier para hacer que el botón tenga una decoración, con un color y borde redondeado
+            //       con el fin de que no se pierda ya que antes de colocar este container el botón casi no se veia.
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(20))),
+              child: IconButton(
+                icon: const Icon(Icons.more_vert_outlined),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
