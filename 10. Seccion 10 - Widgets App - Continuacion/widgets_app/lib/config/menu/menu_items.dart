bab5b161499adebc6cb98f18bfe20ee4c5780fde
Alquimista-07 <jdca.07@gmail.com>
2023-10-22 15:03:20 -0500

    141. Checkbox, Radios y otros Tiles

--- 10. Seccion 10 - Widgets App - Continuacion/widgets_app/lib/config/menu/menu_items.dart
+++ 10. Seccion 10 - Widgets App - Continuacion/widgets_app/lib/config/menu/menu_items.dart
@@ -47,5 +47,11 @@ const appMenuItems = <MenuItem>[
     subtitle: 'Statefull widget animado',
     link: '/animated',
     icon: Icons.check_box_outline_blank_rounded,
+  ),
+  MenuItem(
+    title: 'UI Controls + Tiles',
+    subtitle: 'Una serie de controles de Flutter',
+    link: '/ui-controls',
+    icon: Icons.car_rental_outlined,
   )
 ];
--- 10. Seccion 10 - Widgets App - Continuacion/widgets_app/lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ 10. Seccion 10 - Widgets App - Continuacion/widgets_app/lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -7,6 +7,95 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI Controls'),
+      ),
+      body: const _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView();
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+// NOTA: Esta eumeración me va a servir a mi para saber cuales son los valores permitido en el RadioButton
+enum Transportation { car, plane, boat, submarine }
+
+class _UiControlsViewState extends State<_UiControlsView> {
+  bool isDeveloper = true;
+
+  Transportation selectedTransportation = Transportation.car;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      // NOTA: Esta propiedad ClampingScrollPhysics es el opuesto al BouncingScrollPhysics y la cual nos sirve para quitar el efecto de rebote
+      //       cuando hacemos el scroll
+      physics: const ClampingScrollPhysics(),
+      children: [
+        // NOTA: La ventaja de usar este tipo de SwitchListTile es que básicamente lo tenemos en una lista y la cual si damos click en la lista y no sobre el switch
+        //       este va a cambiar y no tendríamos que dar click directamente sobre el switch
+        SwitchListTile(
+          value: isDeveloper,
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          // NOTA: Cambiamos el estado del switch para que se encienda o se apague
+          onChanged: (value) => setState(() {
+            // IsDeveloper es igual al valor opuesto de isDeveloper
+            isDeveloper = !isDeveloper;
+          }),
+        ),
+
+        RadioListTile(
+          title: const Text('By Car'),
+          subtitle: const Text('Viajar por carro'),
+          value: Transportation.car,
+          // NOTA: La propiedad groupValue es la que nosotros usamos o vamos a usar para marcar o obtener el valor de cual es la opción seleccionada
+          groupValue: selectedTransportation,
+          // NOTA: En el onChanged nosotros vamos a actualizar el estado
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.car;
+          }),
+        ),
+        RadioListTile(
+          title: const Text('By Boat'),
+          subtitle: const Text('Viajar por barco'),
+          value: Transportation.boat,
+          // NOTA: La propiedad groupValue es la que nosotros usamos o vamos a usar para marcar o obtener el valor de cual es la opción seleccionada
+          groupValue: selectedTransportation,
+          // NOTA: En el onChanged nosotros vamos a actualizar el estado
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.boat;
+          }),
+        ),
+        RadioListTile(
+          title: const Text('By Plane'),
+          subtitle: const Text('Viajar por avión'),
+          value: Transportation.plane,
+          // NOTA: La propiedad groupValue es la que nosotros usamos o vamos a usar para marcar o obtener el valor de cual es la opción seleccionada
+          groupValue: selectedTransportation,
+          // NOTA: En el onChanged nosotros vamos a actualizar el estado
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.plane;
+          }),
+        ),
+        RadioListTile(
+          title: const Text('By Submarine'),
+          subtitle: const Text('Viajar por Submarino'),
+          value: Transportation.submarine,
+          // NOTA: La propiedad groupValue es la que nosotros usamos o vamos a usar para marcar o obtener el valor de cual es la opción seleccionada
+          groupValue: selectedTransportation,
+          // NOTA: En el onChanged nosotros vamos a actualizar el estado
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.submarine;
+          }),
+        ),
+      ],
+    );
   }
 }
