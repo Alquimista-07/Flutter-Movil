41cd3d9464c620a383a7c999f4733ffe1442677e
Alquimista-07 <jdca.07@gmail.com>
2023-10-25 12:23:29 -0500

    172. Entidad - Repositories y Datasources

---
+++ 12. Seccion 12 - Full App - Cinemapedia/cinemapedia/lib/domain/datasources/movies_datasource.dart
@@ -0,0 +1,14 @@
+// NOTA: Acá no voy a tener las implementaciones ya que acá en datasources es como quiero
+//       que sean los origenes de datos. Y por lo tanto el objetivo de esta clase es que
+//       sea abstracta ya que no quiero crear instancias de ella.
+//
+//       Adicionalmente acá en el datasource voy a definir como lucen los origenes de datos
+//       que pueden traer peliculas, puede ser de MovieDB, IMDB, mi API propia, un JSON de
+//       donde sea.
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+abstract class MovieDatasource {
+  // Peliculas actualmente en cartelera
+  Future<Movie> getNowPlaying({int page = 1});
+}
