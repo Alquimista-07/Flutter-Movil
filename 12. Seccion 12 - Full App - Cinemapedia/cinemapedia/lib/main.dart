98a9352e31eb6d46b23a8dbad0518bd4e6560e3b
Alquimista-07 <jdca.07@gmail.com>
2023-10-26 09:18:23 -0500

    174. Environment Variables y Git

--- 12. Seccion 12 - Full App - Cinemapedia/cinemapedia/lib/main.dart
+++ 12. Seccion 12 - Full App - Cinemapedia/cinemapedia/lib/main.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 
-void main() {
+// NOTA: Importamos el paquete de dotenv que nos ayuda a manejar las variables de entorno, que instalamos y configuramos en nuestro pubspec.yaml
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// NOTA: A continuación para usar dotenv debemos convertir el manin en un Future que no retorna nada
+Future<void> main() async {
+  // NOTA: A continuación con el uso del await inicializamos el paquete y le indicamos el archivo que contiene las variables de entorno..
+  //       Y luego ya para usarla es sencillo ya que donde necesitemos usarla solo agregamos dotenv.env['VAR_NAME'];
+  await dotenv.load(fileName: '.env');
+
   runApp(const MainApp());
 }
 
