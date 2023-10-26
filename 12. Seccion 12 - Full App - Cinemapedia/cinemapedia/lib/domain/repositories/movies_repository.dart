5ff4c8ceb7a96250994a384f3991baf170b6dd4d
Alquimista-07 <jdca.07@gmail.com>
2023-10-26 18:45:10 -0500

    Refactor para ajustar el tipo que había quedado errado

--- 12. Seccion 12 - Full App - Cinemapedia/cinemapedia/lib/domain/repositories/movies_repository.dart
+++ 12. Seccion 12 - Full App - Cinemapedia/cinemapedia/lib/domain/repositories/movies_repository.dart
@@ -7,5 +7,5 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 
 abstract class MoviesRepository {
   // Peliculas actualmente en cartelera
-  Future<Movie> getNowPlaying({int page = 1});
+  Future<List<Movie>> getNowPlaying({int page = 1});
 }
