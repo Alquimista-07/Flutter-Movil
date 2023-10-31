28bec69c41c3a94d7a9eed873d171935030244f9
Alquimista-07 <jdca.07@gmail.com>
2023-10-31 12:25:48 -0500

    212. Movie Details - Caché Local

---
+++ 14. Seccion 14 - Peliculas individuales y actores/cinemapedia/lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,51 @@
+// NOTA: Creamos un provider que se va a encargar de llamar a la implementación, y OJO esto es importante ya que no es tarea del widget
+//       llamar las implementaciones sino que esa es tarea del provider.
+//       Ahora lo que este provider va a hacer es crear un cache local para evitar hacer peticiones a la API innecesarias si ya habíamos
+//       visualizado la información de una película.
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// NOTA: Creamos el provider
+final movieInfoProvider =
+    StateNotifierProvider<MovieMapNotifier, dynamic>((ref) {
+  final movieRepository = ref.watch(movieRepositoryProvider);
+
+  return MovieMapNotifier(getMovie: movieRepository.getMovieById);
+});
+
+/*
+  La idea es que se consulte el id si ya existe en el mapa entonces devuelve la movie,
+  y si no existe voy a realizar la petición para cargar la información.
+  {
+    '502342': Movie(),
+    '503543': Movie(),
+    '504444': Movie(),
+    '504545': Movie(),
+  }
+*/
+
+// NOTA: Como yo voy a esperar una función que específicamente regrese algo, puedo crearme un nuevo typedef
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallback getMovie;
+
+  // Constructor que regresa un mapa vacío
+  MovieMapNotifier({required this.getMovie}) : super({});
+
+  Future<void> loadMovie(String movieId) async {
+    // No voy a cargar nada si ya la tiene, adicionalmente podríamos colocar una fecha y si ya la fecha cambio traemos
+    // la data nuevamente dese la API y habrían muchas cosas que podríamos hacer
+    if (state[movieId] != null) return;
+
+    // Si no la tenemos pedimos la data de la película a la API.
+    // NOTA: Pero notemos que acá aún no estoy usando la implementación, simplemente estoy definiendolo
+    final movie = await getMovie(movieId);
+
+    // Y acá viene lo interesante que es hacer la actualización al estado, pero de nuevo voy a crear un nuevo voy a generar
+    // un nuevo estado clonando el estado haciendo el spred del estado anterior, y luego el movieId va a apuntar a la movie
+    state = {...state, movieId: movie};
+  }
+}
--- 14. Seccion 14 - Peliculas individuales y actores/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ 14. Seccion 14 - Peliculas individuales y actores/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   static const name = 'movie-screen';
 
   final String movieId;
@@ -8,10 +8,24 @@ class MovieScreen extends StatelessWidget {
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+  // NOTA: Vamos a usar el initstate para saber cuando estoy cargando, cuando termino de cargar, entre otras
+  //       cosas. Adicionalmente vamos a manejar un cache local para también mostrar un loading y para hacer
+  //       más eficiente la aplicación y no hacer una petición nuevamente si ya anteriormente habíamos ingresado
+  //       a ver la información de esa película
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MovieID: $movieId'),
+        title: Text('MovieID: ${widget.movieId}'),
       ),
     );
   }
