5ca07b63c86c65b6fdf6e30edd088899c63ef470
Alquimista-07 <jdca.07@gmail.com>
2023-11-01 09:41:20 -0500

    214. Diseño de la pantalla de película

--- 14. Seccion 14 - Peliculas individuales y actores/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ 14. Seccion 14 - Peliculas individuales y actores/cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -51,8 +51,90 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MovieID: ${widget.movieId}'),
+      // NOTA: Como quiero trabajar con slivers voy a usar un CustomScrollView
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppBar(movie: movie),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomSliverAppBar extends StatelessWidget {
+  final Movie movie;
+  const _CustomSliverAppBar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    // Obtenemos las dimensiones del dispositivo con el fin de más adelante usar el porcentaje que necesitemos
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7, // 70% de la pantalla
+      foregroundColor: Colors.white, // Color del primer plano
+      // NOTA: El flexibleSpace es el epacio flexible del nuestro custom AppBar
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 20),
+          textAlign: TextAlign.start,
+        ),
+        // NOTA: Congiguración del background que en este caso es una imágen con un stack que como sabemos
+        //       este stack nos permite colocar unos widgets encima de otros y la posición depende del orden
+        //       por lo tanto entre más abajo este agregado más al frente va a aestar ese widget
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            // NOTA: Vamos a colocar un gradiente para los posters con color claro
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    // NOTA: El begin y el end los asignamos para que el gradiente sea de arriba hacia abajo
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    // NOTA: Ahora como queremos que el gradiente no inicie deste el tope de la pantalla le decimos
+                    //       con los stops que inicie en el 70% del espacio asignado y termine en el 100% del espacio
+                    //       asignado
+                    stops: [0.7, 1.0],
+                    // NOTA: Asignamos los colore
+                    colors: [
+                      Colors.transparent,
+                      Colors.black87,
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            // NOTA: Vamos a colocar un gradiente en la sección de la flecha del appbar que sirve para regresar ya que de la misma forma
+            //       en algunos posters no se alcanza a apreciar
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    stops: [0.0, 0.3],
+                    colors: [
+                      Colors.black87,
+                      Colors.transparent,
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
