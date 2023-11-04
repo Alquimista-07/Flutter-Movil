47362d470b14741426d77f42d5441d2b166d84b1
Alquimista-07 <jdca.07@gmail.com>
2023-11-04 10:48:56 -0500

    Refactor nombre de archivo

--- 15. Seccion 15 - SearchDelegate - Busquedas/cinemapedia/lib/presentation/providers/providers.dart
+++ 15. Seccion 15 - SearchDelegate - Busquedas/cinemapedia/lib/presentation/providers/providers.dart
@@ -7,4 +7,4 @@ export 'movies/initial_loading_provider.dart';
 export 'actors/actors_by_movie_provider.dart';
 export 'actors/actors_repository_provider.dart';
 
-export 'search/serch_movies_provider.dart';
+export 'search/search_movies_provider.dart';
