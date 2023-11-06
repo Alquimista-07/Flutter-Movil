c4de129c9d2104b23d045e9491997277c444be45
Alquimista-07 <jdca.07@gmail.com>
2023-11-06 17:07:52 -0500

    251. Creación de Vistas

---
+++ 16. Seccion 16 - ShellRoutes - Go Router - Tabs/cinemapedia-v2/lib/presentation/views/movies/favorites_view.dart
@@ -0,0 +1,18 @@
+// NOTA: Esta va a ser una view pero va a ser parcial la cual va a estar dentro de un widget padre, por lo tanto el concepto de un view o vista
+//       no es más que un widget que es muy similar a un widget que sea un screen solo que este es parcial y no ocupa toda la pantalla. Otra cosa
+//       es que la vista puede o no tener un scaffold
+import 'package:flutter/material.dart';
+
+class FavoritesView extends StatelessWidget {
+  const FavoritesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorites View'),
+      ),
+      body: const Center(child: Text('Favoritos')),
+    );
+  }
+}
---
+++ 16. Seccion 16 - ShellRoutes - Go Router - Tabs/cinemapedia-v2/lib/presentation/views/views.dart
@@ -0,0 +1,4 @@
+// NOTA: Archivo de barril
+
+export 'movies/favorites_view.dart';
+export 'movies/home_view.dart';
