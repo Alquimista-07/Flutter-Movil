1686d019ad91b7f2ab1152d914d9ba0b78c8da9e
Alquimista-07 <jdca.07@gmail.com>
2023-11-06 18:12:15 -0500

    252. Configuración del Router

--- 16. Seccion 16 - ShellRoutes - Go Router - Tabs/cinemapedia-v2/lib/config/router/app_router.dart
+++ 16. Seccion 16 - ShellRoutes - Go Router - Tabs/cinemapedia-v2/lib/config/router/app_router.dart
@@ -2,18 +2,27 @@ import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
-  // NOTA: Definimos la pantalla inicial con el path /
-  initialLocation: '/',
+  // NOTA: Acá para el tema de las otras vistas y el KeepAlive para mantener el estado vamos a tener que hacer algunos ajustes al router
+  initialLocation: '/home/0',
   routes: [
     // NOTA: Ahora como nos dimos cuenta que al tomar la url con el id de la pelicula y navegar directamente por esta (Es decir hacer
     //       depp linking) esto causa que perdamos el botón de ir atrás en el appbar y esto es porque la ruta la teníamos como una ruta
     //       principal.
     //       Por lo tanto para solucionar esto y mantenerlo hacemos una configuración en la ruta principal y agregamos la propiedad routes,
     //       que son conocidos como child routes o rutas hijas y ahí es donde vamos a colocar nuestra ruta.
+    // NOTA: Ahora como queremos hacer que se preserve el estado completo de la vista necesitamos hacer algunos cambios acá en el router
+    //       y también como ahora vamos a tener las otras vistas correspondientes a los tabs necesitamos de alguna forma pasar esas
+    //       referencias a las vistas a través de un parámetro page.
     GoRoute(
       name: HomeScreen.name,
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
+      path: '/home/:page',
+      builder: (context, state) {
+        final pageIndex = state.pathParameters['page'] ?? '0';
+
+        // NOTA: Y recordemos que este index tiene que ser numérico por lo tanto lo parseamos para convertirlo ya que en el
+        //       parámetro viene como un String
+        return HomeScreen(pageIndex: int.parse(pageIndex));
+      },
       routes: [
         // NOTA: Acá vamos a crear una ruta que va a enviar un parámetro para navegar a una película por id
         GoRoute(
--- 16. Seccion 16 - ShellRoutes - Go Router - Tabs/cinemapedia-v2/lib/presentation/screens/movies/home_screen.dart
+++ 16. Seccion 16 - ShellRoutes - Go Router - Tabs/cinemapedia-v2/lib/presentation/screens/movies/home_screen.dart
@@ -5,12 +5,30 @@ import 'package:cinemapedia/presentation/widgets/widgets.dart';
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
-  const HomeScreen({super.key});
+  // NOTA: Ahora como vamos a incluir otras vistas necesitamos de alguna forma pasar que índice correspondiente al tab es al que
+  //       se quiere navegar por lo tanto creamos una propiedad que nos va a ayudar.
+  final int pageIndex;
+
+  const HomeScreen({super.key, required this.pageIndex});
+
+  // NOTA: Creamos un listado de widgets con las vistas
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(), // <----- categorias view
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeView(),
+    return Scaffold(
+      // NOTA: Ahora para el tema de preservar el estado y tener el KeepAlive, existe un widget de Flutter que me sirve para eso que es el
+      //       IndexedStack
+      body: IndexedStack(
+        index: pageIndex,
+        // NOTA: Y el children va a ser toda la colección de widgets que van a servirme para estarlos cambiando
+        //       en base a lo que tenga el pageIndex
+        children: viewRoutes,
+      ),
       // NOTA: Acá vamos a agregar el bottom navigation bar, y sobre el cual podemos ver un ejemplo en el
       //       siguiente enlace: https://www.youtube.com/watch?v=HB5WMcxAmQQ&ab_channel=FernandoHerrera
       //       si queremos saber más de él.
