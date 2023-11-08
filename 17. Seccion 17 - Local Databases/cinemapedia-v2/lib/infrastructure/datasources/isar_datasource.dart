3e40a791bd149077e36b330753d600b7e2db7718
Alquimista-07 <jdca.07@gmail.com>
2023-11-08 16:31:50 -0500

    265. Implementar métodos necesarios

--- 17. Seccion 17 - Local Databases/cinemapedia-v2/lib/infrastructure/datasources/isar_datasource.dart
+++ 17. Seccion 17 - Local Databases/cinemapedia-v2/lib/infrastructure/datasources/isar_datasource.dart
@@ -41,20 +41,56 @@ class IsarDatasource extends LocalStorageDatasource {
   }
 
   @override
-  Future<bool> isMovieFavorite(int movieId) {
-    // TODO: implement isMovieFavorite
-    throw UnimplementedError();
+  Future<bool> isMovieFavorite(int movieId) async {
+    // Esperamos que la base de datos este lista
+    final isar = await db;
+
+    // NOTA: OJO acá lo definimos como opcional ya que puede que la película este en favoritos como puede que no.
+    //       Entonces vamos a decirle a isar que quiero que haga un filtro en mi esquema de movies. En este caso
+    //       usamos un método autogenerado en nuestro esquema y que nos va a servir par comporara los ids. Dichos
+    //       métodos autogenerados va a iniciar con el nombre de alguna propiedad de nuestra entity seguido de lo
+    //       que hace y esto es precisamente para que sea fácil para nostros poder usarlos, adicionalmente si
+    //       cambiaramos algo en nuestra entity tendríamos que generar nuevamente el Schema.
+    final Movie? isFavoriteMovie =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+
+    // Si es diferente de null regresa true en caso contrario false
+    return isFavoriteMovie != null;
   }
 
+  // NOTA: Para el paginado hacemos la condicion donde el offset sea igual al offset recibido y el limite sea igual al limite recibido
+  //       nos va a traer todo, por lo tanto cada vez que actualicemos el offset va a hacer referencia a este por ejemplo en la primera
+  //       trae del 1 al 10, la siguiente del 11 al 20 y así sucesivamente.
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await db;
+
+    return isar.movies.where().offset(offset).limit(limit).findAll();
   }
 
+  // NOTA: Para el tema del toggle es un poco más elavorado ya que acá ocupamos aplicar o realizar una transacción para que se escriba, actualice
+  //       elimine o lea en la base de datos, es decir, hacer interaciones con la base de datos. Adicionalmente si se quiere hacer muchas interaciones
+  //       a la vez esto se puede hacer dentro de una misma transacción. Para más información tenemos a disposición la documentación de Isar.
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
-    throw UnimplementedError();
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await db;
+
+    // NOTA: Recordemos que es un toogle por lo tanto si esta en la base de datos la borramos y si no esta la insertamos.
+    final favoriteMovie =
+        await isar.movies.filter().idEqualTo(movie.id).findFirst();
+
+    if (favoriteMovie != null) {
+      // Borrar
+      // NOTA: Este es similar al de insertar y le pasamos el isarId y usamos es el isarId porque esta indexado y sabemos cual es por lo tanto
+      //       como ya sabemos que lo tenemos le indicamos el ! para que no marque error.
+      //       Además en este punto cuando obtenemos el favoriteMovie ya hemos hecho la busqueda por lo tanto sabemos que lo tenemos.
+      isar.writeTxnSync(() => isar.movies.deleteSync(favoriteMovie.isarId!));
+      return;
+    }
+
+    // Caso contrario insertar.
+    // NOTA: Acá tenemos el método writeTxn que es transacción normal y el writeTxnSync que es síncrono. Entonces en este caso usamo el sync
+    //       y le indicamos la colección donde lo queremos insertar y le pasamos el método put
+    isar.writeTxnSync(() => isar.movies.putSync(movie));
   }
 }
