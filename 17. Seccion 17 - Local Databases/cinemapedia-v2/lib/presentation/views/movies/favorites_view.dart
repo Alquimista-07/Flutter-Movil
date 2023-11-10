d1122947cb8f3313a0768c1291c7e7967ac2b65d
Alquimista-07 <jdca.07@gmail.com>
2023-11-10 09:21:53 -0500

    271. Resolución de la tarea

--- 17. Seccion 17 - Local Databases/cinemapedia-v2/lib/presentation/views/movies/favorites_view.dart
+++ 17. Seccion 17 - Local Databases/cinemapedia-v2/lib/presentation/views/movies/favorites_view.dart
@@ -1,18 +1,47 @@
 // NOTA: Esta va a ser una view pero va a ser parcial la cual va a estar dentro de un widget padre, por lo tanto el concepto de un view o vista
 //       no es más que un widget que es muy similar a un widget que sea un screen solo que este es parcial y no ocupa toda la pantalla. Otra cosa
 //       es que la vista puede o no tener un scaffold
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+//* Tarea clase 271
+// NOTA: Es necesario primero convertir en un statefulwidget para la luego nuevamente convertirlo en un consumerstatefulwidget
+//       esto con el fin de que al crear el statefulwidget nos cree automáticamete el método createState. Adicionalmente la
+//       razón de convertirlo en un consumerstatefulwidget es porque necesitamos tener acceso al state notifier del provider de
+//       favoritos de riverpod.
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  //* Tarea clase 271
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    //* Tarea clase 271
+    // NOTA: Como el favoriteMovies es un mapa<int, movie> entonces esto lo necesitamos transformar a una lista
+    //       y esto podemos hacerlo con un ciclo for, pero hay una forma más sencilla que es tomando el values y
+    //       convirtiendolo a una lista con el método toList()
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
+      //* Tarea clase 271
+      body: ListView.builder(
+        itemCount: favoriteMovies.length,
+        itemBuilder: (context, index) {
+          final favorites = favoriteMovies[index];
+          return ListTile(title: Text(favorites.title));
+        },
       ),
-      body: const Center(child: Text('Favoritos')),
     );
   }
 }
