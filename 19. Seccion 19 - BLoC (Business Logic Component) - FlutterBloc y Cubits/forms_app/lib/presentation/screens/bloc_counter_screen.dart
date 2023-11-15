09c5f9f9ad3f194c91b8ee6e126ce7830c466c49
Alquimista-07 <jdca.07@gmail.com>
2023-11-15 16:27:50 -0500

    291. Utilizar Counter Bloc

--- 19. Seccion 19 - BLoC (Business Logic Component) - FlutterBloc y Cubits/forms_app/lib/presentation/screens/bloc_counter_screen.dart
+++ 19. Seccion 19 - BLoC (Business Logic Component) - FlutterBloc y Cubits/forms_app/lib/presentation/screens/bloc_counter_screen.dart
@@ -1,8 +1,37 @@
+// NOTA: Para consumir el Bloc es muy muy muy similar a Cubit y la llamada de los métodos es idéntica
+//       pero usualmente cuando nosotros queremos un cambio en el estado emitimos un evento y esa es la
+//       diferencia.
+//       Por lo tanto al ser similar a Cubit la forma de consumir y leer los valores con el read, el watch y demás
+//       cosas que vimos en Cubit son iguales
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_bloc/counter_bloc.dart';
 
 class BlocCounterScreen extends StatelessWidget {
   const BlocCounterScreen({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    // NOTA: De la misma forma que Cubit tengo que envolver los widget dentro del provider para tener acceso
+    //       a la instancia del BLoC. Adicionalmente este provider tiene el context pero en este caso no lo
+    //       necesito entonces lo dejo con _
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: const BlocCounterView(),
+    );
+  }
+}
+
+class BlocCounterView extends StatelessWidget {
+  const BlocCounterView({
+    super.key,
+  });
+
+  void increasedCounterBy(BuildContext context, [int value = 1]) {
+    // NOTA: Como no creamos un método increasedBy del lado del Bloc como teniamos con el Cubit acá vamos es a usar el método
+    //       .add() el cual me permite disparar un evento y el cual en nuestro BLoC si tenemos un evento.
+    context.read<CounterBloc>().add(CounterIncreased(value));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +44,11 @@ class BlocCounterScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Counter value: 000'),
+      body: Center(
+        child: context.select(
+          (CounterBloc counterBlock) =>
+              Text('Counter value: ${counterBlock.state.counter}'),
+        ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -26,19 +58,19 @@ class BlocCounterScreen extends StatelessWidget {
             //       FloatingActionButton que se anima por defecto entre Scaffolds
             heroTag: '1',
             child: const Text('+3'),
-            onPressed: () => {},
+            onPressed: () => increasedCounterBy(context, 3),
           ),
           const SizedBox(height: 15),
           FloatingActionButton(
             heroTag: '2',
             child: const Text('+2'),
-            onPressed: () => {},
+            onPressed: () => increasedCounterBy(context, 2),
           ),
           const SizedBox(height: 15),
           FloatingActionButton(
             heroTag: '3',
             child: const Text('+1'),
-            onPressed: () => {},
+            onPressed: () => increasedCounterBy(context),
           ),
         ],
       ),
