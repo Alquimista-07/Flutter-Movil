6b7f51b8fb2beebe50bab1f144894c310d7a2003
Alquimista-07 <jdca.07@gmail.com>
2023-11-16 12:03:41 -0500

    298. Consideraciones con inputs y scroll

--- 20. Seccion 20 - Manejo de formularios/forms_app/lib/presentation/screens/register_screen.dart
+++ 20. Seccion 20 - Manejo de formularios/forms_app/lib/presentation/screens/register_screen.dart
@@ -19,6 +19,38 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // NOTA: Ojo hay que considerar la ubicación de los campos de texto, adicionalmente podemos trabajar con el SafeArea
+    //       para asegurarnos de mostrar el widget basdo en el notch, controles de movimiento, etc. Pero este safeArea no
+    //       soluciona todo, por lo tanto tenemos que tener mucho cuidado ya que puede llegar a darse overflow y causar
+    //       problemas al visualizar los widgets, y por lo tanto se recomienda envolver en algún widget que permita hacer
+    //       scroll como un listview, un singlechildscrollview, etc.
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const FlutterLogo(size: 100),
+              // NOTA: Este widget nos permite colocar los campos de texto, que luego se le agregara sus validaciones y demás
+              //       cosas que tiene que tener un formulario.
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+              TextFormField(),
+
+              const SizedBox(height: 20),
+
+              FilledButton.tonalIcon(
+                onPressed: () {},
+                icon: const Icon(Icons.save),
+                label: const Text('Crear Usuario'),
+              ),
+
+              const SizedBox(height: 20)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
