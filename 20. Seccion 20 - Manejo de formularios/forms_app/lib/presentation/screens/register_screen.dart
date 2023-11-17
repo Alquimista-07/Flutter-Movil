c56f35e1efc0dc1f4ab87ff0ef16b27a28c94255
Alquimista-07 <jdca.07@gmail.com>
2023-11-17 10:08:12 -0500

    301. Formulario tradicional

--- 20. Seccion 20 - Manejo de formularios/forms_app/lib/presentation/screens/register_screen.dart
+++ 20. Seccion 20 - Manejo de formularios/forms_app/lib/presentation/screens/register_screen.dart
@@ -51,39 +51,99 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  // NOTA: Vamos a crear el globalkey que me permite enlazar el key con el formkey
+  //       y que me va a permitir tener el control del formulario basado en ese key
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formKey,
         child: Column(
-      children: [
-        // NOTA: Acá vamos a usar nuestro widget personalizado
-        CustomTextFormField(
-          label: 'Nombre De Usuario',
-        ),
+          children: [
+            // NOTA: Acá vamos a usar nuestro widget personalizado
+            CustomTextFormField(
+              label: 'Nombre De Usuario',
+              // NOTA: Recordemos que cada uno de nuestro CustomTextFormField tiene el onChange que nos pemite captuar el valor de ese input
+              onChanged: (value) => username = value,
+              // NOTA: Recordemnos que ocupamos hacer las validaciones
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Campo requerido';
+                // Recordemos que el trim quita espacios adelante y atrás
+                if (value.trim().isEmpty) return 'Campo requerido';
+                if (value.length < 6) return 'Más de 6 letras';
+                return null;
+              },
+            ),
 
-        const SizedBox(height: 10),
+            const SizedBox(height: 10),
 
-        CustomTextFormField(
-          label: 'Correo Electrónico',
-        ),
+            CustomTextFormField(
+              label: 'Correo Electrónico',
+              // NOTA: Recordemos que cada uno de nuestro CustomTextFormField tiene el onChange que nos pemite captuar el valor de ese input
+              onChanged: (value) => email = value,
+              // NOTA: Recordemnos que ocupamos hacer las validaciones
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Campo requerido';
+                // Recordemos que el trim quita espacios adelante y atrás
+                if (value.trim().isEmpty) return 'Campo requerido';
 
-        const SizedBox(height: 10),
+                // NOTA: Validamos con una expresión regular para verificar si tiene formato de correo
+                final emailRegExp = RegExp(
+                  r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                );
+                if (!emailRegExp.hasMatch(value)) {
+                  return 'No tiene formato de correo';
+                }
 
-        CustomTextFormField(
-          label: 'Contraseña',
-          obscureText: true,
-        ),
+                return null;
+              },
+            ),
 
-        const SizedBox(height: 20),
-        FilledButton.tonalIcon(
-          onPressed: () {},
-          icon: const Icon(Icons.save),
-          label: const Text('Crear Usuario'),
-        ),
-      ],
-    ));
+            const SizedBox(height: 10),
+
+            CustomTextFormField(
+              label: 'Contraseña',
+              obscureText: true,
+              // NOTA: Recordemos que cada uno de nuestro CustomTextFormField tiene el onChange que nos pemite captuar el valor de ese input
+              onChanged: (value) => password = value,
+              // NOTA: Recordemnos que ocupamos hacer las validaciones
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Campo requerido';
+                // Recordemos que el trim quita espacios adelante y atrás
+                if (value.trim().isEmpty) return 'Campo requerido';
+                if (value.length < 6) return 'Más de 6 letras';
+                return null;
+              },
+            ),
+
+            const SizedBox(height: 20),
+            FilledButton.tonalIcon(
+              onPressed: () {
+                // NOTA: Ahora para que se apliquen las validaciones a nuestros campos podríamos tomar la referencia a cada campo y hacer la referencia al validator,
+                //       pero sale más fácil hacer referencia directamnete al formulario usando el formKey
+                final isValid = _formKey.currentState!.validate();
+                if (!isValid) return;
+
+                // NOTA: Cuando haga click en el botón imprimo los valores de los campos
+                print('{$username, $email, $password}');
+              },
+              icon: const Icon(Icons.save),
+              label: const Text('Crear Usuario'),
+            ),
+          ],
+        ));
   }
 }
