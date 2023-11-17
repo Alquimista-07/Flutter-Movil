a426ead47daf84a6c9b2eafeeb30f1d2903db5c1
Alquimista-07 <jdca.07@gmail.com>
2023-11-17 13:53:11 -0500

    303. Register Form Cubit

---
+++ 20. Seccion 20 - Manejo de formularios/forms_app/lib/presentation/blocs/register/register_cubit.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterFormState> {
+  RegisterCubit() : super(const RegisterFormState());
+
+  void onSubmit() {
+    print('Submit: ${state}');
+  }
+
+  // Métodos para cuando cambien los valores del username, email y password
+  void usernameChanged(String value) {
+    emit(state.copyWith(
+      username: value,
+    ));
+  }
+
+  void emailChanged(String value) {
+    emit(state.copyWith(
+      email: value,
+    ));
+  }
+
+  void passwordChanged(String value) {
+    emit(state.copyWith(
+      password: value,
+    ));
+  }
+}
