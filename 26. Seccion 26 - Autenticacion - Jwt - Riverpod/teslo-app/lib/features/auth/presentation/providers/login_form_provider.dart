38a6234783116f9fe853882b72018ae8f53d0c99
Alquimista-07 <jdca.07@gmail.com>
2023-11-29 17:15:31 -0500

    369. LoginForm Provider y Notifier

--- 26. Seccion 26 - Autenticacion - Jwt - Riverpod/teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
+++ 26. Seccion 26 - Autenticacion - Jwt - Riverpod/teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
 //! 1 - State del provider
@@ -16,8 +18,85 @@ class LoginFormState {
     this.email = const Email.pure(),
     this.password = const Password.pure(),
   });
+
+  LoginFormState copyWith({
+    bool? isPosting,
+    bool? isFormPosted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) =>
+      LoginFormState(
+        isPosting: isPosting ?? this.isPosting,
+        isFormPosted: isFormPosted ?? this.isFormPosted,
+        isValid: isValid ?? this.isValid,
+        email: email ?? this.email,
+        password: password ?? this.password,
+      );
+
+  @override
+  String toString() {
+    return '''
+      LoginFormState
+      isPosting: $isPosting
+      isFormPosted: $isFormPosted
+      isValid: $isValid
+      email: $email
+      password: $password
+    ''';
+  }
 }
 
 //! 2 - Como impementamos un notifier
+// NOTA: Estado para manejdar los campos del login
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  // NOTA: En el super mandamos la creación del estado inicial
+  LoginFormNotifier() : super(LoginFormState());
+
+  onEmailChanged(String value) {
+    final newEmail = Email.dirty(value);
+
+    state = state.copyWith(
+      email: newEmail,
+      isValid: Formz.validate([newEmail, state.password]),
+    );
+  }
+
+  onPasswordChanged(String value) {
+    final newPassword = Password.dirty(value);
+
+    state = state.copyWith(
+      password: newPassword,
+      isValid: Formz.validate([newPassword, state.email]),
+    );
+  }
+
+  onFormSubmit() {
+    _touchEveryField();
+
+    if (!state.isValid) return;
+
+    print(state);
+  }
+
+  // NOTA: Ocupamos este método para que cuando el campo toquemos el botón de ingresar todos los campos queden tocados o sucios,
+  //       es decir, que ya no tengan un estado pure para que esto dispare las validaciones
+  _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    state = state.copyWith(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password]),
+    );
+  }
+}
 
 //! 3 - StateNotifierProvider - Consume afuera
+final loginFormProvider =
+// NOTA: Recordemos que el autoDispose destruye y reinicia la información diligenciada en los campos del formulario.
+    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
