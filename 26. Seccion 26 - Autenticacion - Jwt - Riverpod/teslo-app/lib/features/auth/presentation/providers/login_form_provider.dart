22fabb23ca9c71f778a43af52f73ea458ab18a05
Alquimista-07 <jdca.07@gmail.com>
2023-12-11 16:23:21 -0500

    370. Conectar formulario con Provider

--- 26. Seccion 26 - Autenticacion - Jwt - Riverpod/teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
+++ 26. Seccion 26 - Autenticacion - Jwt - Riverpod/teslo-app/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -2,50 +2,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
-//! 1 - State del provider
-// NOTA: Esto es como va a lucir nuestro state
-class LoginFormState {
-  final bool isPosting;
-  final bool isFormPosted;
-  final bool isValid;
-  final Email email;
-  final Password password;
-
-  LoginFormState({
-    this.isPosting = false,
-    this.isFormPosted = false,
-    this.isValid = false,
-    this.email = const Email.pure(),
-    this.password = const Password.pure(),
-  });
-
-  LoginFormState copyWith({
-    bool? isPosting,
-    bool? isFormPosted,
-    bool? isValid,
-    Email? email,
-    Password? password,
-  }) =>
-      LoginFormState(
-        isPosting: isPosting ?? this.isPosting,
-        isFormPosted: isFormPosted ?? this.isFormPosted,
-        isValid: isValid ?? this.isValid,
-        email: email ?? this.email,
-        password: password ?? this.password,
-      );
-
-  @override
-  String toString() {
-    return '''
-      LoginFormState
-      isPosting: $isPosting
-      isFormPosted: $isFormPosted
-      isValid: $isValid
-      email: $email
-      password: $password
-    ''';
-  }
-}
+//! 3 - StateNotifierProvider - Consume afuera
+final loginFormProvider =
+// NOTA: Recordemos que el autoDispose destruye y reinicia la información diligenciada en los campos del formulario.
+    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
 
 //! 2 - Como impementamos un notifier
 // NOTA: Estado para manejdar los campos del login
@@ -94,9 +56,47 @@ class LoginFormNotifier extends StateNotifier<LoginFormState> {
   }
 }
 
-//! 3 - StateNotifierProvider - Consume afuera
-final loginFormProvider =
-// NOTA: Recordemos que el autoDispose destruye y reinicia la información diligenciada en los campos del formulario.
-    StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
-  return LoginFormNotifier();
-});
+//! 1 - State del provider
+// NOTA: Esto es como va a lucir nuestro state
+class LoginFormState {
+  final bool isPosting;
+  final bool isFormPosted;
+  final bool isValid;
+  final Email email;
+  final Password password;
+
+  LoginFormState({
+    this.isPosting = false,
+    this.isFormPosted = false,
+    this.isValid = false,
+    this.email = const Email.pure(),
+    this.password = const Password.pure(),
+  });
+
+  LoginFormState copyWith({
+    bool? isPosting,
+    bool? isFormPosted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) =>
+      LoginFormState(
+        isPosting: isPosting ?? this.isPosting,
+        isFormPosted: isFormPosted ?? this.isFormPosted,
+        isValid: isValid ?? this.isValid,
+        email: email ?? this.email,
+        password: password ?? this.password,
+      );
+
+  @override
+  String toString() {
+    return '''
+      LoginFormState
+      isPosting: $isPosting
+      isFormPosted: $isFormPosted
+      isValid: $isValid
+      email: $email
+      password: $password
+    ''';
+  }
+}
