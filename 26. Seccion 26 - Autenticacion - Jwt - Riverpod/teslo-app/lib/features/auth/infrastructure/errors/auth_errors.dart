ce882ef437dc1f1fe3abaf9cf087199f3c627ec6
Alquimista-07 <jdca.07@gmail.com>
2023-12-13 17:23:19 -0500

    378. Manejo de errores

--- 26. Seccion 26 - Autenticacion - Jwt - Riverpod/teslo-app/lib/features/auth/infrastructure/errors/auth_errors.dart
+++ 26. Seccion 26 - Autenticacion - Jwt - Riverpod/teslo-app/lib/features/auth/infrastructure/errors/auth_errors.dart
@@ -1,5 +1,14 @@
 // NOTA: Acá vamos a tener nuestros errores personalizados
 
+class ConnectionTimeout implements Exception {}
+
 class WrongCredentials implements Exception {}
 
 class InvalidToken implements Exception {}
+
+class CustomError implements Exception {
+  final String message;
+  final int errorCode;
+
+  CustomError(this.message, this.errorCode);
+}
