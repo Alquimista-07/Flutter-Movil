daf682c1d94e6696b580eac21cfe4e3476d99dc0
Alquimista-07 <jdca.07@gmail.com>
2023-12-13 12:24:48 -0500

    376. Login y Logout desde el provider

--- 26. Seccion 26 - Autenticacion - Jwt - Riverpod/teslo-app/lib/features/auth/presentation/providers/auth_provider.dart
+++ 26. Seccion 26 - Autenticacion - Jwt - Riverpod/teslo-app/lib/features/auth/presentation/providers/auth_provider.dart
@@ -18,11 +18,41 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   AuthNotifier({required this.authRepository}) : super(AuthState());
 
-  void loginUser(String email, String password) async {}
+  void loginUser(String email, String password) async {
+    // NOTA: Simulamos un retardo para poder apreciar bien el proceso, pero esto no es necesario colocarlo en la vida real
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    try {
+      final user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } on WrongCredentials {
+      logout('Credenciales no son correctas');
+    } catch (e) {
+      logout('Error no controlado');
+    }
+  }
+
+  Future<void> logout([String? errorMessage]) async {
+    // TODO: Limpiar token
+
+    state = state.copyWith(
+        authStatus: AuthStatus.notAuthenticated,
+        user: null,
+        errorMessage: errorMessage);
+  }
 
   void registerUser(String email, String password, String fullName) async {}
 
   void checkAuthStatus() async {}
+
+  void _setLoggedUser(User user) {
+    // TODO: Necesito guardar el token fisicamente
+
+    state = state.copyWith(
+      user: user,
+      authStatus: AuthStatus.authenticated,
+    );
+  }
 }
 
 enum AuthStatus { checking, authenticated, notAuthenticated }
