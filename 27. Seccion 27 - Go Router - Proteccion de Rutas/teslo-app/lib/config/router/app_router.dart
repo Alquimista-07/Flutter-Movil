e8bf54787ce7bd35513601b14a0535d2ce2e16ba
Alquimista-07 <jdca.07@gmail.com>
2023-12-15 12:12:48 -0500

    391. Go_Router - Protección de Rutas

--- 27. Seccion 27 - Go Router - Proteccion de Rutas/teslo-app/lib/config/router/app_router.dart
+++ 27. Seccion 27 - Go Router - Proteccion de Rutas/teslo-app/lib/config/router/app_router.dart
@@ -1,32 +1,48 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/splash',
-  routes: [
-    //* Primera pantalla
-    GoRoute(
-      path: '/splash',
-      builder: (context, state) => const CheckAuthStatusScreen(),
-    ),
+// NOTA: Con la ayuda de reverpod vamos a crear un provider para envolver las rutas y de esta forma ayudarnos con la protección de las rutas dependiendo si estoy
+//       autenticado o no , incluso esto se presta para que podamos evitar la construcción de ciertas rutas si no se está autenticado simila a un lazyload en Angular
+//       y sería un beneficio de hacerlo de esta forma.
+//       OJO Hay una nebulosa con este tema de la protección de rutas ya que en móvil no tenemos una forma de cambiar la ruta a través de una url como se haría en web,
+//       a no ser de que tengamos un deep linking o algo parecido y por eso entra en controversia si usar la protección de rutas o no en móvil cosa que en web si es
+//       super importante implementarlo, pero en móvil no esta de más agregarlo.
+//       Adicionalmente esta forma de proteger rutas nos sirve tanto para web, móvil, escritorio, etc.
+final goRouterProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      //* Primera pantalla
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
 
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+      ///* Auth Routes
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
+      ///* Product Routes
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
 
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+    // NOTA: Bloquear si no se está autenticado de alguna manera
+    // NOTA: Ya con esto de envolver en un provider yo ya tengo acceso a si estoy autenticado, si no estoy autenticado, etc, etc,.
+    //       y donde vamos a tener que jugar con una propiedad llamada redirect para navegar entre pantallas dependiendo a dicha
+    //       autenticación
+    redirect: (context, state) {
+      return null;
+    },
+  );
+});
--- 27. Seccion 27 - Go Router - Proteccion de Rutas/teslo-app/lib/main.dart
+++ 27. Seccion 27 - Go Router - Proteccion de Rutas/teslo-app/lib/main.dart
@@ -11,11 +11,16 @@ void main() async {
   ));
 }
 
-class MainApp extends StatelessWidget {
+// NOTA: Ahora como nuestras rutas las envolvimos en un provider con el fin de ayudarnos a tener acceso para saber si estoy autenticad o no y de esta forma tener la protección de rutas, entonces
+//       tenemos que transformar en un ConsumerWidget para tener la referencia al ref y mandar llamar ese provider
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // NOTA: Referencia al provider que envuelve nuestras rutas
+    final appRouter = ref.watch(goRouterProvider);
+
     return MaterialApp.router(
       routerConfig: appRouter,
       theme: AppTheme().getTheme(),
