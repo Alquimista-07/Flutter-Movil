385b124c937a63af47f16782534b41744dbc7f34
Alquimista-07 <jdca.07@gmail.com>
2023-12-18 10:03:51 -0500

    405. Riverpod - StateNotifierProvider - Notifier

--- 28. Seccion 28 - Obtener productos  - Datasources y Repositories/teslo-app/lib/features/products/presentation/providers/products_provider.dart
+++ 28. Seccion 28 - Obtener productos  - Datasources y Repositories/teslo-app/lib/features/products/presentation/providers/products_provider.dart
@@ -1,6 +1,52 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+// NOTIFIER
+class ProductsNotifier extends StateNotifier<ProductsState> {
+  final ProductsRepository productsRepository;
+
+  ProductsNotifier({
+    required this.productsRepository,
+  }) :
+        // NOTA: Cuando se cree creamos la instancia de nuestro state y llamamos el método leadNextPage inmediatamente
+        super(ProductsState()) {
+    loadNextPage();
+  }
+
+  Future loadNextPage() async {
+    // NOTA: Para evitar el bomabardeo de peticiones al backend hacemos validaciones ya que esto sucede mucho cuando tenemos un inifinite scroll
+    if (state.isLoading || state.isLastPage) return;
+
+    // Caso contrario actualizamos el state para indicar que esta cargando
+    state = state.copyWith(isLoading: true);
+
+    final products = await productsRepository.getProductsByPage(
+      limit: state.limit,
+      offset: state.offset,
+    );
+
+    // NOTA: Puede que luego de hacer la petición los products vengan vacíos por lo tanto validamos si ya no tenemos productos
+    //       por lo tanto actualizamos el estado para por ejemplo que deje de cargar y hacer peticiónes al backend, etc.
+    if (products.isEmpty) {
+      state = state.copyWith(
+        isLoading: false,
+        isLastPage: true,
+      );
+      return;
+    }
+
+    // NOTA: Pero si tenemos más productos
+    state = state.copyWith(
+        isLastPage: false,
+        isLoading: false,
+        offset: state.offset + 10,
+        // NOTA: Con los productos hacemos un spread de los productos que tenía anteriormente en el estado y el spred de los nuevos productos
+        products: [...state.products, ...state.products]);
+  }
+}
+
 // STATE
 // NOTA: El state es como quiero qu luzca la información del provider
-import 'package:teslo_shop/features/products/domain/domain.dart';
 
 class ProductsState {
   final bool isLastPage;
@@ -17,13 +63,13 @@ class ProductsState {
     this.products = const [],
   });
 
-  ProductsState copyWith(
+  ProductsState copyWith({
     bool? isLastPage,
     int? limit,
     int? offset,
     bool? isLoading,
     List<Product>? products,
-  ) =>
+  }) =>
       ProductsState(
         isLastPage: isLastPage ?? this.isLastPage,
         limit: limit ?? this.limit,
