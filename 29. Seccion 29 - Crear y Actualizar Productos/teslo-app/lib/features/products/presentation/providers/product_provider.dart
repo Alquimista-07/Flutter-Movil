efa69811d21954b2c67c8bef2c4a916fc010cdea
Alquimista-07 <jdca.07@gmail.com>
2023-12-19 16:10:17 -0500

    415. Product Provider

---
+++ 29. Seccion 29 - Crear y Actualizar Productos/teslo-app/lib/features/products/presentation/providers/product_provider.dart
@@ -0,0 +1,61 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+import 'products_repository_provider.dart';
+
+// PROVIDER
+// NOTA: Recorudemos que el autodispose sirve para que se limpie cada vez que ya no se va a utilizar
+//       y el family se usa para esperar un valor al momento de utilizar el provider ya que se necesita
+//       el id.
+//       Adicionalmente como tenemos el family este va a esperar tres argumentos que sería el notifier, el state
+//       y un String correspondiente al tipo del id del producto.
+final productProvider = StateNotifierProvider.autoDispose
+    .family<ProductNotifier, ProductState, String>((ref, productId) {
+  // NOTA: Recordemos que el productsRepository la ventaja es que cuando nos autentiquemos y todo lo demás este se va a refrescar solo y va a esparcir el token en todos los lugares
+  //       donde lo utilizo, siempre y cuando este centralizado y lo usemos en ese productsRepository
+  final productsRepository = ref.watch(productsRepositoryProvider);
+
+  return ProductNotifier(
+    productsRepository: productsRepository,
+    productId: productId,
+  );
+});
+
+// NOTIFIER
+class ProductNotifier extends StateNotifier<ProductState> {
+  final ProductsRepository productsRepository;
+
+  ProductNotifier({
+    required this.productsRepository,
+    required String productId,
+  }) : super(ProductState(id: productId));
+
+  Future<void> loadProduct() async {}
+}
+
+// STATE
+class ProductState {
+  final String id;
+  final Product? product;
+  final bool isLoading;
+  final bool isSaving;
+
+  ProductState({
+    required this.id,
+    this.product,
+    this.isLoading = true,
+    this.isSaving = false,
+  });
+
+  ProductState copyWith({
+    String? id,
+    Product? product,
+    bool? isLoading,
+    bool? isSaving,
+  }) =>
+      ProductState(
+        id: id ?? this.id,
+        product: product ?? this.product,
+        isLoading: isLoading ?? this.isLoading,
+        isSaving: isSaving ?? this.isSaving,
+      );
+}
