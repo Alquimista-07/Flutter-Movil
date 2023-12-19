1692f809b04da406884352fb3b2a382bd978ac8d
Alquimista-07 <jdca.07@gmail.com>
2023-12-19 18:13:35 -0500

    417. Product Screen - Fix

--- 29. Seccion 29 - Crear y Actualizar Productos/teslo-app/lib/features/products/presentation/screens/product_screen.dart
+++ 29. Seccion 29 - Crear y Actualizar Productos/teslo-app/lib/features/products/presentation/screens/product_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/products/presentation/providers/providers.dart';
 
-class ProductScreen extends ConsumerStatefulWidget {
+// NOTA: Recordemos que el ConsumerWidget es la versión homologa al StatelessWidget y el ConsumerStatefullWidget es la homolga al StatefullWidget
+class ProductScreen extends ConsumerWidget {
   final String productId;
 
   const ProductScreen({
@@ -11,29 +12,39 @@ class ProductScreen extends ConsumerStatefulWidget {
   });
 
   @override
-  ProductScreenState createState() => ProductScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    // NOTA: Como el productProvider es un provider que tiene el modificador family() lo tenemos que llamar colocando los () y pasarle el argumento que este reciba que en este caso es el id del producto.
+    // NOTA: OJOOOOOOOOOOOOO NOTA IMPORTANTE: Si este fuera un ConsumerStatefullWidget y no un ConsumerWidget y fueramos a usar el productProvider también este provider al tener el modificador family()
+    //       si necesitaramos acceder al .notifier inmediatamente como lo hemos hecho con otros providers de riverpod que hemos creado anteriormente, por lo tanto, tenemos que mandarlo a llamar colocando
+    //       los () y pasarle el argumento que este reciba que en este caso es el id del producto y luego si podemos mandar a llamar el .notifier
+    //       Adicionalmente recordemos que usaríamos widget para acceder a una propiedad debido a que estoy dentro de un StatefullWidget o ConsumerStatefulWidget
+    //
+    //       Ejemplo:
+    //
+    //       ref.read(productProvider(widget.productId).notifier);
+    //
+    final productState = ref.watch(productProvider(productId));
 
-class ProductScreenState extends ConsumerState<ProductScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // NOTA: Como el productProvider es un provider que tiene el modificador family() por lo tanto no podemos acceder al .notifier inmediatamente
-    //       como lo hacemos hecho con otros providers de riverpod que hemos creado anteriormente, por lo tanto, tenemos que mandarlo a llamar
-    //       colocando los () y pasarle el argumento que este reciba que en este caso es el id del producto y luego si podemos mandar a llamar el
-    //       .notifier
-    // NOTA: Recordemos que usamos widget para acceder a una propiedad debido a que estoy dentro de un StatefullWidget o ConsumerStatefulWidget
-    ref.read(productProvider(widget.productId).notifier);
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Editar Producto'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.camera_alt_outlined),
+          )
+        ],
       ),
       body: Center(
-        child: Text(widget.productId),
+        child: Text(productState.product?.title ?? 'Cargando'),
+      ),
+      // NOTA: Algo que es chévere es que como tenemos un floatinActionButton en la pantalla de productos y acá agregamos otro, al cambiar entre pantallas
+      //       este va a hacer una transición con una animación por defecto de forma automática y a esto se le conoce como un Hero animation en Flutter.
+      //       Adicionalmente si queremos conocer más sobre este Hero Animation podemor ver video de Fernando que se encuentra en el siguiente enlace:
+      //       https://www.youtube.com/watch?v=8IO6eqcTjNc&ab_channel=FernandoHerrera
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.save_as_outlined),
       ),
     );
   }
